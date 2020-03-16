9733ca42f3b6570f322b46f721dfcce57f217ee9
rp775 <54200028+rp775@users.noreply.github.com>
2020-03-16 02:31:37 -0400

    Update openjdkchoco.ps1
    UPDATED STS AND ANT

--- pkgs/openjdkchoco.ps1
+++ pkgs/openjdkchoco.ps1
@@ -57,3 +57,72 @@ $installDir = "$programFiles\OpenJDK"
 
 Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
 rm -r "$installDir\openjdk-11.0.6_10"
+
+
+
+
+
+Spring Tool Suite 3.9.6
+install
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$ant_home = Join-Path $package 'apache-ant-1.10.5'
+$ant_bat = Join-Path $ant_home 'bin/ant.bat'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'ant' `
+    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.zip' `
+    -Checksum '2E48F9E429D67708F5690BC307232F08440D01EBE414059292B6543971DA9C7CD259C21533B9163B4DD753321C17BD917ADF8407D03245A0945FC30A4E633163' `
+    -ChecksumType 'SHA512' `
+    -UnzipLocation $package
+
+Install-ChocolateyEnvironmentVariable `
+    -VariableName 'ANT_HOME' `
+    -VariableValue $ant_home `
+    -VariableType 'Machine'
+
+Install-BinFile -Name 'ant' -Path $ant_bat
+
+Update-SessionEnvironment
+
+uninstall
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutPath = $(Join-Path $DesktopPath "STS.lnk")
+
+if ($(Test-Path $ShortcutPath) -eq $true) {
+    Remove-Item -Path $ShortcutPath
+}
+
+
+ANT
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$ant_home = Join-Path $package 'apache-ant-1.10.5'
+$ant_bat = Join-Path $ant_home 'bin/ant.bat'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'ant' `
+    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.zip' `
+    -Checksum '2E48F9E429D67708F5690BC307232F08440D01EBE414059292B6543971DA9C7CD259C21533B9163B4DD753321C17BD917ADF8407D03245A0945FC30A4E633163' `
+    -ChecksumType 'SHA512' `
+    -UnzipLocation $package
+
+Install-ChocolateyEnvironmentVariable `
+    -VariableName 'ANT_HOME' `
+    -VariableValue $ant_home `
+    -VariableType 'Machine'
+
+Install-BinFile -Name 'ant' -Path $ant_bat
+
+Update-SessionEnvironment
+
+UNINSTALL
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$ant_home = Join-Path $package 'apache-ant-1.10.5'
+$ant_bat = Join-Path $ant_home 'bin/ant.bat'
+
+Uninstall-BinFile -Name 'ant' -Path $ant_bat
