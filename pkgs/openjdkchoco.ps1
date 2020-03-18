59a98bf78de9e113ce95a1d7bff0f960631f826c
rp775 <54200028+rp775@users.noreply.github.com>
2020-03-17 22:18:55 -0400

    Update openjdkchoco.ps1
    proxy

--- pkgs/openjdkchoco.ps1
+++ pkgs/openjdkchoco.ps1
@@ -301,3 +301,12 @@ $ShortcutPath = $(Join-Path $DesktopPath "STS.lnk")
 if ($(Test-Path $ShortcutPath) -eq $true) {
     Remove-Item -Path $ShortcutPath
 }
+
+
+
+
+choco config set proxy http://localhost:8888
+choco config set proxyUser bob
+choco config set proxyPassword 123Sup#rSecur3
+choco config set proxyBypassList "'http://localhost,http://this.location/'" #0.10.4 required
+choco config set proxyBypassOnLocal true #0.10.4 required
